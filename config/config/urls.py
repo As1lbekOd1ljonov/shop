80d30f74ec883dc13d8603c878409699cbce0bb5
As1lbekOd1ljonov <aslbekoo176@gmail.com>
2025-03-13 00:53:05 +0500

    vazifa

--- config/config/urls.py
+++ config/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from config.config import settings
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- config/main/views.py
+++ config/main/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpRequest
 from django.shortcuts import render
 
+from .models import Category, Product, ProductImage
+
 
 def index(request: HttpRequest):
-    return render(request,"index.html")
+    category = Category.objects.all()
+    product = Product.objects.all()
+    product_image = ProductImage.objects.all()
+
+    context = {
+        "category": category,
+        "product": product,
+        "product_image": product_image,
+    }
+
+    return render(request,"index.html", context)
+
+
